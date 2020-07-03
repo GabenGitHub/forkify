7c4f5722cd0bf0057ca0bea8378255168438fe92
Gabor Szabo <gabor.szabo86@hotmail.com>
2020-07-04 00:23:33 +0200

    Fix API

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,21 +1,19 @@
 import axios from "axios";
-import { key, proxy } from '../config'
 
 export default class Search {
-    constructor(query) {
-        this.query = query;
-    }
+  constructor(query) {
+    this.query = query;
+  }
 
-    async getResult() {
-        try {
-            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
-            this.result = res.data.recipes;
-            // console.log(this.result);
-        } catch(error) {
-            console.log(error);
-        }
+  async getResult() {
+    try {
+      const res = await axios(
+        `https://forkify-api.herokuapp.com/api/search?&q=${this.query}`
+      );
+      this.result = res.data.recipes;
+      // console.log(this.result);
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
-
-
-
